74fe4b5063f7e76d8c0207c140cc7b9a81f2a6eb
2321387 <43662351+2321387@users.noreply.github.com>
2025-09-05 17:16:10 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -121,13 +121,11 @@ const API_SITES = {
     testSource: {
         api: 'https://91jpzyw.com/api.php/provide/vod',
         name: '91',
-        adult: true
-    },
-    testSource: {
         api: 'https://hsckzy888.com/api.php/provide/vod',
         name: '黄色仓库',
         adult: true
     },
+
     //ARCHIVE https://telegra.ph/APIs-08-12
 };
 
